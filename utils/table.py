147767ba022c7f4c90a959b2db6e76694241e209
Sieg <csiegried@yahoo.fr>
2026-05-28 16:12:51 +0200

    update 16h20

--- utils/table.py
+++ utils/table.py
@@ -1,3 +1,19 @@
+class Seat:
+    def __init__(self):
+        self.free = True
+        self.occupant = None
+
+    def set_occupant(self, name: str):
+        self.occupant = name
+        self.free = False
+
+    def remove_occupant(self):
+        name = self.occupant
+        self.occupant = None
+        self.free = True
+        return name
+
+
 class Table:
     def __init__(self, capacity: int = 4):
         self.seats = [Seat() for _ in range(capacity)]
@@ -9,5 +25,5 @@ class Table:
                 return True
         return False
 
-    def left_capacity(self):
+    def left_capacity(self) -> int:
         return sum(1 for seat in self.seats if seat.free)
